70a8609347696a40c21a210a5a315d8f4547a57f
simonjeger <simonjeger@gmail.com>
2025-10-02 08:18:46 +0200

    Changed config

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ STATE = {
     'TRACKING': 3
 }
 STATE_LOOKUP = {v: k for k, v in STATE.items()}
-DT = 0.1          # execution period in seconds (0.04 without the wind sensor, 0.12 with it) (can be set to 0.06666666 if save_images is False)
+DT = 0.1          # execution period in seconds (0.04 without the wind sensor, 0.12 with it) (set to 0.1 for save_images = 0, 0.2 for save_images = 1 or 2)
 DT_VISION = 0.1   # execution period for vision in seconds (7Hz) (can be set to 0.06666666 if save_images is False)
 
 
@@ -47,30 +47,30 @@ ZIPLINE_LENGTH_CALIB = 1500                             # in meters
 P_GAIN_VISION = 0.5
 I_GAIN_VISION = 0.0
 D_GAIN_VISION = 0.0
-MAX_CNT_MOVING_BLINDLY = 7          # number of iterations applying last detected position without any new detection
-CUT_OFF_FREQUENCY_TRACKING = 5      # in Hz
-CATCH_UP_GAIN = 1.2                 # 3 working well with acc of 3 and max speed of 11
+MAX_CNT_MOVING_BLINDLY = 7                              # number of iterations applying last detected position without any new detection
+CUT_OFF_FREQUENCY_TRACKING = 5                          # in Hz
+CATCH_UP_GAIN = 1.2                                     # 3 working well with acc of 3 and max speed of 11
 
 
 # LEDs
-NUM_LEDS = 2                        # number of LEDs
-RED = (255, 0, 0)                   # RGB colors for red
-BLUE = (0, 0, 255)                  # RGB colors for blue    
-GREEN = (0, 255, 0)                 # RGB colors for green
-YELLOW = (255, 255, 0)              # RGB colors for yellow
-PURPLE = (102, 51, 153)             # RGB colors for purple
+NUM_LEDS = 2                                            # number of LEDs
+RED = (255, 0, 0)                                       # RGB colors for red
+BLUE = (0, 0, 255)                                      # RGB colors for blue
+GREEN = (0, 255, 0)                                     # RGB colors for green
+YELLOW = (255, 255, 0)                                  # RGB colors for yellow
+PURPLE = (102, 51, 153)                                 # RGB colors for purple
 
 
 # RC communication
-BUTTON_PIN = 24                     # GPIO24 (PIN 18)
-TROTTLE_PIN = 25                    # GPIO25 (PIN 22)
-STEERING_PIN = 22                   # GPIO22 (PIN 15)
-PWM_MIN_PULSE_WIDTH = 1050          # in µs
-PWM_DEFAULT_PULSE_WIDTH = 1500      # in µs
-PWM_MAX_PULSE_WIDTH = 1950          # in µs
-GO_STOP_THRESHOLD = 200             # in µs
-BUTTON_TOGGLE_THRESHOLD = 200       # in µs
-CALIB_SETPOINTS_THRESHOLD = 100     # in µs
+BUTTON_PIN = 24                                         # GPIO24 (PIN 18)
+TROTTLE_PIN = 25                                        # GPIO25 (PIN 22)
+STEERING_PIN = 22                                       # GPIO22 (PIN 15)
+PWM_MIN_PULSE_WIDTH = 1050                              # in µs
+PWM_DEFAULT_PULSE_WIDTH = 1500                          # in µs
+PWM_MAX_PULSE_WIDTH = 1950                              # in µs
+GO_STOP_THRESHOLD = 200                                 # in µs
+BUTTON_TOGGLE_THRESHOLD = 200                           # in µs
+CALIB_SETPOINTS_THRESHOLD = 100                         # in µs
 REMOTE_COMMAND = {
     'GO_STOP': 0,
     'GO_BACKWARD': 1,
@@ -90,33 +90,33 @@ CAM_WIDTH_LOW = int(CAM_HEIGHT_LOW / ASPECT_RATIO)  # lower resolution settings,
 FRAME_RATE = 60                                     # in frames/sec
 CALIBRATION_SQUARE = 0.0323                         # size of the squares in the checkerboard, in meters
 CHECKERBOARD_SHAPE = (4, 7)                         # number of inner corners per row and column !!nb of squares - 1!!
-NB_IMAGES_CALIBRATION = 300                          # number of images to capture for calibration
+NB_IMAGES_CALIBRATION = 300                         # number of images to capture for calibration
 ARUCO_DICT = cv.aruco.DICT_4X4_50                   # 5x5 dictionary with 250 unique markers
 ARUCO_ID = 7                                        # exact marker ID to be detected between 0 and 249
 ARUCO_PIXEL_SIZE = 400                              # size of the ArUco marker, in pixels (for ArUo generation)
 ARUCO_REAL_SIZE = 0.136                             # size of the ArUco marker, in meters (to be measured in real life)
-EXPOSURE_TIME = 4000                                  # in microseconds (set 29 µs for outside, and 3000 µs for inside)
+EXPOSURE_TIME = 4000                                # in microseconds (set 29 µs for outside, and 3000 µs for inside)
 ANALOGUE_GAIN = 2.0                                 # in dB
-SAVE_IMAGES = 0                                 # save images during operation (0, no images saved, 1 save raw images, 2 save process images)
+SAVE_IMAGES = 0                                     # save images during operation (0, no images saved, 1 save raw images, 2 save process images)
 
 # Marker detection
-AUTO_EXPOSURE = True                               # Enable auto exposure (used to be False)
-RECENTER_ORIGIN = False                              #  If we alter the center of the image (used to be True)
-ADVANCED_PARAMETERS = True                         # Enable advanced parameters for ArUco detection (used to be False)
-PRE_PROCESS = True                                 # Enable pre-processing of the image (used to be False)
+AUTO_EXPOSURE = True                                # Enable auto exposure (used to be False)
+RECENTER_ORIGIN = False                             #  If we alter the center of the image (used to be True)
+ADVANCED_PARAMETERS = True                          # Enable advanced parameters for ArUco detection (used to be False)
+PRE_PROCESS = True                                  # Enable pre-processing of the image (used to be False)
 SOLVER = 2                                          # 0: standard solver, 1: PnP solver, 2: PnP solver with initial guess as last pose
 CROP_X = 0.0                                        # percentage of image to crop (0.1 = 10%)
 CROP_Y = 0.0                                        # percentage of image to crop (0.1 = 10%)
-RES_DROP_PRE = 1.0                                 # resolution drop factor for pre-processing (used to be 1.0)
+RES_DROP_PRE = 1.0                                  # resolution drop factor for pre-processing (used to be 1.0)
 UNDISTORT = True                                    # set False to work in distorted (raw) domain
 STARTING_OFFSET = 0.0                               # starting offset applied at the beginning and end of the zipeline, in meters
 
 # Wind sensor
 SERIAL_PORT_LI550= '/dev/ttyUSB0'
-BAUD_RATE_LI550 = 115200                # in bps
-WIND_AXIS_LENGTH = 10                   # in meters/s
-NUM_PAST_VECTORS = 10                   # number of past vector displayed
-INIT_TIME_LI550 = 10                    # time to wait for the LI550 to be ready, in seconds
+BAUD_RATE_LI550 = 115200                            # in bps
+WIND_AXIS_LENGTH = 10                               # in meters/s
+NUM_PAST_VECTORS = 10                               # number of past vector displayed
+INIT_TIME_LI550 = 10                                # time to wait for the LI550 to be ready, in seconds
 LI550_MAPPING = {
     'S': 'Wind 3D norm [m/s]',
     'S2': 'Wind 2D norm [m/s]',
